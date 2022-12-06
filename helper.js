57748b87d05522f10e9885eff37ccdaef43dfa9b
Patrik Ahlström <github@tray2.se>
2022-12-06 14:21:41 +0100

    Initial commit

---
+++ helper.js
@@ -0,0 +1,99 @@
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+
+function getAllElements(selector) {
+  return document.querySelectorAll(selector);
+}
+
+function getValue(selector) {
+  return getElement(selector).value;
+}
+
+function setValue(selector, value) {
+  getElement(selector).value = value;
+}
+
+function getInnerHTML(selector) {
+  return getElement(selector).InnerHTML;
+}
+
+function setInnerHTML(selector, html) {
+  getElement(selector).InnerHTML = html;
+}
+
+function show(selector) {
+  getElement(selector).classList.remove('is-hidden');
+}
+
+function hide(selector) {
+  getElement(selector).classList.add('is-hidden');
+}
+
+function showLoading() {
+  show('#loading');
+}
+
+function hideLoading() {
+  hide('#loading');
+}
+
+function isHidden(selector) {
+  return getElement(selector).classList.contains('is-hidden');
+}
+
+function isVisible(selector) {
+  return ! getElement(selector).classList.contains('is-hidden');
+}
+
+function hasClass(selector, cssClass) {
+  return getElement(selector).classList.contains(cssClass);
+}
+
+async function getRequest(url, parameters = {}, loading = true) {
+  try {
+      if (loading) {
+          showLoading();
+      }
+      let responsePromise = await fetch(url, { method: 'GET', headers: parameters});
+      if(! responsePromise.ok) {
+          if(loading) {
+              hideLoading();
+          }
+          throw new Error(responsePromise.status );
+      }
+      if (loading) {
+          hideLoading();
+      }
+      return await responsePromise.json();
+  } catch (error) {
+      return error;
+  }
+} 
+
+async function postRequest(url, parameters = {}, loading = true) {
+  try {
+      if (loading) {
+          showLoading();
+      }
+      let responsePromise = await fetch(url, { 
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: new URLSearchParams(parameters)
+      });
+      if(! responsePromise.ok) {
+          if (loading) {
+              hideLoading();
+          }
+          throw new Error(responsePromise.status );
+      }
+      if (loading) {
+          hideLoading();
+      }
+      return await responsePromise.json();
+  } catch (error) {
+      return error;
+  }
+}
